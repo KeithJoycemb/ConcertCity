2e62f9f8fb3bffac9293ba551e13c746b489f14f
Stephen Harding <D00157833@student.dkit.ie>
2023-03-13 11:35:25 +0000

    audio on pick ups added , new background audio added
    Changed the pick up script to play an  audio scouce on pick up , this was tricky as it was destroying the audio before it could play. also added in new background audio to which im hopping to have turn on once we get to 10 collectables

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -7,8 +7,21 @@ public class Collectible : MonoBehaviour
 {
     public static event Action OnCollected;
     public static int total;
+    public AudioClip pickupSound;
+    private MeshRenderer meshRenderer;
+    private Collider collider;
+    private Light light;
+
+    private AudioSource audioSource;
 
     void Awake() => total++;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        collider = GetComponent<Collider>();
+        light = GetComponent<Light>();
+    }
     void Update()
     {
         transform.localRotation = Quaternion.Euler(90f, Time.time * 100f, 0);
@@ -16,10 +29,23 @@ public class Collectible : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        // This checks if its the player who collided with it then disables aeverything attacked to the object except the audio, and once its done playing it destroys the object
         if(other.CompareTag("Player"))
         {
+            
+
             OnCollected?.Invoke();
-            Destroy(gameObject);
+            audioSource.PlayOneShot(pickupSound);
+            meshRenderer.enabled = false;
+            collider.enabled = false;
+            light.enabled = false;
+            StartCoroutine(DestroyAfterSound());
         }
+    }
+
+    IEnumerator DestroyAfterSound()
+    {
+        yield return new WaitForSeconds(pickupSound.length);
+        Destroy(gameObject);
     }
 }
