4be7426fde8027e7d43070bf30bc0d56bea7d188
Stephen Harding <D00157833@student.dkit.ie>
2023-04-26 17:47:42 +0100

    Win condition implemented and win scene completed
    Created the win condiotion inside the collectable script which loads the win screen. i created a scene for this and also a new cheering animtion for the main character

--- Assets/Scripts/collectibleCount.cs
+++ Assets/Scripts/collectibleCount.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class collectibleCount : MonoBehaviour
 {
     TMPro.TMP_Text text;
     int count;
+    private int totalCollectibles;
+    private bool hasWon = false;
 
     private void Awake()
     {
 
         text = GetComponent<TMPro.TMP_Text>();
+        totalCollectibles = FindObjectsOfType<Collectible>().Length;
     }
 
     void start() => UpdateCount();
@@ -22,6 +26,13 @@ public class collectibleCount : MonoBehaviour
     {
         count++;
         UpdateCount();
+
+        if (count == totalCollectibles && !hasWon)
+        {
+            hasWon = true;
+            Debug.Log("You have collected all the collectibles!");
+            SceneManager.LoadScene(2);
+        }
     }
     void UpdateCount()
     {
